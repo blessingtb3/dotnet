51a205347dcb71feebdd61bf38ef012cf494f346
blessingtb3 <blessingtb3@gmail.com>
2025-02-07 14:42:58 +0200

    feat: add TodoDbContext class and installation of packages in TodoAPI.csproj

---
+++ TodoAPI/AppDataContext/TodoDbContext.cs
@@ -0,0 +1,35 @@
+//AppDataContext/TodoDBContext.cs
+using DBSettings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TodoAPI.models;
+
+namespace TodoDbContext.AppDataContext{
+
+    //TodoDbContext class inherits from DbContext
+    public class TodoDbContext : DbContext{
+
+        //DbSettings field to store the connection string
+        private readonly DbSettings _dbsettings;
+
+        //Constructor to inject DbSettings model
+        public TodoDbContext(IOptions<DbSettings> dbSettings){
+            _dbsettings = dbSettings.Value;
+        }
+
+        //DbSet property to represent the Todo table
+        public DbSet<Todo> Todos { get; set; }
+
+        //Configuring the database provider and connection string 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseSqlServer(_dbsettings.ConnectionString);
+        }
+
+        //Confuring our model for the Todo entity 
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            modelBuilder.Entity<Todo>()
+                .ToTable("TodoAPI")
+                .HasKey(x => x.Id);
+        }
+    }
+}
---
+++ TodoAPI/models/DbSettings.cs
@@ -0,0 +1,9 @@
+//Models/DbSettings.cs
+namespace DBSettings{
+
+    public class DbSettings{
+
+        public string ConnectionString { get; set; } //store the actual connection string value
+   
+    }
+}
