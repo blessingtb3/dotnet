69ca0718f0f89edc6dd78c70c0b3ffbd54afb88d
blessingtb3 <blessingtb3@gmail.com>
2025-02-22 19:06:48 +0200

    feat: add implementation of CreateTodoAsync method in the controller class

--- TodoAPI/Controllers/README.md
+++ TodoAPI/Controllers/README.md
@@ -3,3 +3,6 @@ In ASP.NET Core, controllers are responsible for handling incoming HTTP requests
 
 # Todo Controller class
 - The TodoController class inherits from ControllerBase, a base class provided by ASP.NET Core for creating controllers. We've also added a route prefix of api/[controller] to the controller, which will be used as the base route for all actions in the controller.
+
+# After, Implement the CreateTodoAsync Method in the TodoController Class
+- Now that we have our Controller class, let's implement the CreateTodoAsync method in the TodoController class. This method will handle the creation of new Todo items in our database.
--- TodoAPI/Controllers/Todo.Controller.cs
+++ TodoAPI/Controllers/Todo.Controller.cs
@@ -3,11 +3,13 @@
 //serving as the entry point for our API, defining the routes and actions that clients can interact with.
 
 using Microsoft.AspNetCore.Mvc;
+using TodoAPI.Contracts;
 using TodoAPI.Interface;
 
 namespace TodoAPI.Controllers{
     [ApiController ]
     [Route("api/[controller]")]//adding a route prefix to the controller which will be used as the base route for all the actions in the controller
+
     public class TodoController : ControllerBase{//TodoController class inherits from ControllerBase, Base class from ASP.NET Core for creating controllers
 
         //properties/fields
@@ -16,6 +18,22 @@ namespace TodoAPI.Controllers{
         //constructor
         public TodoController(ITodoServices todoServices){
             _todoServices = todoServices;
+        }
+
+        //Implementing CreateTodoAsync method to handle the creation of new Todo items in our db
+        [HttpPost]
+        public async Task<IActionResult> CreateTodoAsync(CreateTodoRequest request){
+
+            if(!ModelState.IsValid){//checeking if the request model is valid
+                return BadRequest(ModelState);//returning a bad request response with the model state errors
+            }
+
+            try{
+                await _todoServices.CreateTodoAsync(request);//calling CreateTodoAsync method from the ITodoServices interface to create a new Todo item in the db
+                return Ok(new { message = "Blog post successfully created"});//returning a success response for the creation of a todo item
+            }catch(Exception ex){
+                return StatusCode(500, new { message = "An error occurred while creating the blog post", error = ex.Message});
+            }//returning a server error response if an exception occurs while creating the todo item
         }
     }
 }
